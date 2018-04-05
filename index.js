50bde3710b9e793300f9f090e6d8e77a94fd8464
hungrated <zhang295415658@qq.com>
2018-04-05 23:32:10 +0800

    master: javascript test code

---
+++ index.js
@@ -0,0 +1,14 @@
+let exec = require('child_process').exec;
+
+let arg1 = 'hello',
+  arg2 = 'world',
+  fileName = 'src/test.py';
+
+exec(`python ${fileName} ${arg1} ${arg2}`, function (err, stdout, stderr) {
+  if (err) {
+    console.log('stderr:: ' + err);
+  }
+  if (stdout) {
+    console.log(stdout);
+  }
+});
